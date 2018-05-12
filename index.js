c82b7b08749020844e315b0abda0b0d2376bb08e
MrXyfir <mr@xyfir.com>
2018-05-11 19:19:02 -0700

    Add reddit-banhammer

---
+++ index.js
@@ -0,0 +1,29 @@
+const snoowrap = require('snoowrap');
+const CONFIG = require('./config');
+
+(async function() {
+  const r = new snoowrap(CONFIG.SNOOWRAP);
+
+  const page = await r
+    .getSubreddit('UniversalScammerList')
+    .getWikiPage('banlist')
+    .fetch();
+  const sub = await r.getSubreddit(CONFIG.SUBREDDIT);
+
+  for (let line of page.content_md.split('\n')) {
+    const user = line
+      .trim()
+      .substr(5)
+      .split(' ')[0]
+      .split('#')[0];
+
+    if (!user) continue;
+
+    try {
+      await sub.banUser({ name: user });
+      console.log('Banned', user);
+    } catch (err) {
+      console.error('Could not ban user', user, err);
+    }
+  }
+})();
